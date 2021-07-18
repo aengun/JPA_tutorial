62991f09111d020389c1e44bec53790513a0e68e
aengun <aengun@icloud.com>
2021-07-18 14:57:06 +0900

    jpql, criteria

---
+++ ex12-jpql/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,59 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+//            //=========================================================
+//            //jpql
+//            List<Member> result = em.createQuery(
+//                    "select m from Member as m where m.username like '%kim%'",
+//                    Member.class
+//            ).getResultList();
+
+//            //=========================================================
+//            //criteria : 컴파일시점 오타 찾기 쉬움, 동적쿼리 짜기 쉬움 / 유지보수 어려움, 쿼리같지 않음
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//
+//            Root<Member> m = query.from(Member.class);
+//
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+//            List<Member> resultList = em.createQuery(cq).getResultList();
+
+            //=========================================================
+            //
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            System.out.println("e = " + e);
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+
+}
