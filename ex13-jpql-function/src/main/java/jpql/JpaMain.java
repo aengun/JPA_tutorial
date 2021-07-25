80dea87e6a783170e961742f0c57e717ffa014ad
aengun <aengun@icloud.com>
2021-07-25 21:33:17 +0900

    엔티티 직접 사용

--- ex13-jpql-function/src/main/java/jpql/JpaMain.java
+++ ex13-jpql-function/src/main/java/jpql/JpaMain.java
@@ -247,10 +247,97 @@ public class JpaMain {
 //                }
 //            }
 
+//            //===============================================
+//            // 페이징
+//            // Team 클래스에 batchsize
+//            // or Global option으로 default_batch_fetch_size(persistence.xml)
+//            Team teamA = new Team();
+//            teamA.setName("팀A");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("팀B");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("회원1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("회원2");
+//            member2.setTeam(teamA);
+//            em.persist(member2);
+//
+//            Member member3 = new Member();
+//            member3.setUsername("회원3");
+//            member3.setTeam(teamB);
+//            em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
+//
+//            String query = "select distinct t From Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(2)
+//                    .getResultList();
+//
+//            System.out.println("result = " + result.size());
+//
+//            for (Team team : result) {
+//                System.out.println("team : " + team.getName() + ", " + team.getMembers().size());
+//                for (Member m : team.getMembers()) {
+//                    System.out.println("==> member : " + m);
+//                }
+//            }
+
+//            //===============================================
+//            // 엔티티 직접 사용
+//            Team teamA = new Team();
+//            teamA.setName("팀A");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("팀B");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("회원1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("회원2");
+//            member2.setTeam(teamA);
+//            em.persist(member2);
+//
+//            Member member3 = new Member();
+//            member3.setUsername("회원3");
+//            member3.setTeam(teamB);
+//            em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
+//
+////            String query = "select m from Member m where m = :member";
+////
+////            Member findMember = em.createQuery(query, Member.class)
+////                    .setParameter("member", member1)
+////                    .getSingleResult();
+//
+//            // 엔티티 직접 사용한 것과 기본키를 사용한것과 쿼리가 같음
+//            String query = "select m from Member m where m.id = :memberId";
+//
+//            Member findMember = em.createQuery(query, Member.class)
+//                    .setParameter("memberId", member1.getId())
+//                    .getSingleResult();
+//
+//            System.out.println("findMember : " + findMember);
+
             //===============================================
-            // 페이징
-            // Team 클래스에 batchsize
-            // or Global option으로 default_batch_fetch_size(persistence.xml)
+            // 엔티티 직접 사용 - 외래키
             Team teamA = new Team();
             teamA.setName("팀A");
             em.persist(teamA);
@@ -277,21 +364,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select distinct t From Team t join fetch t.members";
+            String query = "select m from Member m where m.team = :team";
 
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
+            List<Member> result = em.createQuery(query, Member.class)
+                    .setParameter("team", teamA)
                     .getResultList();
 
-            System.out.println("result = " + result.size());
-
-            for (Team team : result) {
-                System.out.println("team : " + team.getName() + ", " + team.getMembers().size());
-                for (Member m : team.getMembers()) {
-                    System.out.println("==> member : " + m);
-                }
-            }
+            System.out.println("result : " + result);
 
 
             tx.commit();
