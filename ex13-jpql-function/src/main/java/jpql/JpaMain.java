3bfb98ce33c30f39405549e52b6dc06523ad037a
aengun <aengun@icloud.com>
2021-07-25 21:15:08 +0900

    fetch join

--- ex13-jpql-function/src/main/java/jpql/JpaMain.java
+++ ex13-jpql-function/src/main/java/jpql/JpaMain.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.List;
@@ -122,30 +124,174 @@ public class JpaMain {
 //                    .getResultList();
 //            System.out.println("result = " + result);
 
+//            //===============================================
+//            // fetch Join
+//            Team teamA = new Team();
+//            teamA.setName("팀A");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("팀B");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("회원1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("회원2");
+//            member2.setTeam(teamA);
+//            em.persist(member2);
+//
+//            Member member3 = new Member();
+//            member3.setUsername("회원3");
+//            member3.setTeam(teamB);
+//            em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
+//
+////            String query = "select m From Member m";
+//            String query = "select m From Member m join fetch m.team";
+//
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member : " + member.getUsername() + ", team : " + member.getTeam().getName());
+//                //쿼리 순서
+//                //회원1, 팀A(SQL)
+//                //회원2, 팀A(1차 캐시 )
+//                //회원3, 팀B(SQL)
+//                //100명이라면???? ==> n+1 ==> fetch join으로 해결
+//            }
+
+//            //===============================================
+//            // fetch Join
+//            Team teamA = new Team();
+//            teamA.setName("팀A");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("팀B");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("회원1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("회원2");
+//            member2.setTeam(teamA);
+//            em.persist(member2);
+//
+//            Member member3 = new Member();
+//            member3.setUsername("회원3");
+//            member3.setTeam(teamB);
+//            em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
+//
+//            String query = "select t From Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .getResultList();
+//
+//            for (Team team : result) {
+//                System.out.println("team : " + team.getName() + ", " + team.getMembers().size());
+//                for (Member m : team.getMembers()) {
+//                    System.out.println("==> member : " + m);
+//                }
+//            }
+
+//            //===============================================
+//            // fetch Join - 중복 제거
+//            Team teamA = new Team();
+//            teamA.setName("팀A");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("팀B");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("회원1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("회원2");
+//            member2.setTeam(teamA);
+//            em.persist(member2);
+//
+//            Member member3 = new Member();
+//            member3.setUsername("회원3");
+//            member3.setTeam(teamB);
+//            em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
+//
+//            String query = "select distinct t From Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .getResultList();
+//
+//            for (Team team : result) {
+//                System.out.println("team : " + team.getName() + ", " + team.getMembers().size());
+//                for (Member m : team.getMembers()) {
+//                    System.out.println("==> member : " + m);
+//                }
+//            }
+
             //===============================================
-            // fetch Join
-            Team team = new Team();
-            em.persist(team);
+            // 페이징
+            // Team 클래스에 batchsize
+            // or Global option으로 default_batch_fetch_size(persistence.xml)
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            //명시적 조인으로 별칭을 얻을 후 필요한 속성들을 가져온다.
-            String query = "select m.username from Team t join t.members m";
+            String query = "select distinct t From Team t join fetch t.members";
 
-            List<Collection> result = em.createQuery(query, Collection.class)
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
-            System.out.println("result = " + result);
+
+            System.out.println("result = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team : " + team.getName() + ", " + team.getMembers().size());
+                for (Member m : team.getMembers()) {
+                    System.out.println("==> member : " + m);
+                }
+            }
 
 
             tx.commit();
